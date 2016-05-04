add5509389eb2d039e60cdea92db89d5b1eb18c4
Nancy Majors <nmajors@gmail.com>
2016-05-04 15:06:12 -0500

    added markers to locations

--- app/modules/locations/controller.js
+++ app/modules/locations/controller.js
@@ -1,36 +1,73 @@
 class LocationsController {
-  constructor($state, UserService, LocationsService) {
+  constructor($state, UserService, LocationsService, $scope) {
     this._$state = $state;
     this._LocationsService = LocationsService;
     this._UserService = UserService;
     this.newLocation = this._LocationsService.new();
 
-    this.map = { center: { latitude: 34.7476539, longitude: -92.2650471 }, zoom: 8 };
-    this.markers = [];
+    this.map = {
+      center: {
+        latitude: 32.7476539,
+        longitude: -82.2650471
+      },
+      zoom: 8
+    };
 
     this._UserService
       .isLoggedIn()
       .then((response) => {
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.map.center.latitude = position.coords.latitude;
+          this.map.center.longitude = position.coords.longitude;
+          $scope.$digest();
+        })
+
         this.user = response;
         // console.log(this.user);
-        this.locations = this._LocationsService.getLocations(this.user);
+        this._LocationsService.getLocations(this.user)
+          .then((response) => {
+            this.locations = response;
+            this.showMarkers();
+          });
       })
       .catch((error) => {
         this._$state.go("login");
       });
   }
 
+    addLocation() {
+      this._LocationsService
+        .createLocation(this.newLocation)
+        .then((response) => {
+          console.log(response);
+          this.locations = response;
+          this.showMarkers();
+        });
+    }
 
-  addLocation() {
-    this._LocationsService.createLocation(this.newLocation);
-    console.log(this.newLocation);
-  }
 
+    logout() {
+      this._UserService.logout();
+      this._$state.go("login");
+    }
 
-  logout() {
-    this._UserService.logout();
-    this._$state.go("login");
-  }
+    showMarkers() {
+      this.markers = [];
+
+      this.locations.forEach((location) => {
+        this.marker = {
+          id: location.$id,
+          coords: {
+            latitude: location.coords.lat,
+            longitude: location.coords.lng
+          },
+          address: `${location.address} ${location.city} ${location.state}`,
+        }
+        this.markers.push(this.marker);
+      });
+    }
+
 
 }
 
--- app/modules/locations/service.js
+++ app/modules/locations/service.js
@@ -1,50 +1,54 @@
 class LocationsService {
-  constructor($firebaseArray) {
+  constructor($firebaseArray, $q) {
     this._$firebaseArray = $firebaseArray;
+    this._$q = $q;
 
     this.ref = new Firebase("https://nms-severe.firebaseio.com/");
-
     this.geocoder = new google.maps.Geocoder();
 
-
+    this.markers = [];
   }
 
   //get locations for logged in user
   getLocations(user) {
     this.locations = this._$firebaseArray(this.ref.child('users').child(user.uid).child('locations'));
-    return this.locations;
+    return this.locations.$loaded();
   }
 
   new() {
     return {
       address: "",
       city: "",
-      state: ""
+      state: "",
+      coords: {}
       }
   }
 
   createLocation(location){
-    this.geocoder.geocode( { "address": `${location.address}, ${location.city}, ${location.state}` }, function(results) {
-          console.log(results);
-            let loc = results[0].geometry.location,
-                lat      = loc.lat(),
-                lng      = loc.lng();
-          console.log("Latitude: " + lat);
-          console.log("Longitude: " + lng);
-          this.locations.$add({
-            address: location.address,
-            city: location.city,
-            state: location.state,
-            lat,
-            lng
-          });
-    }.bind(this));
+    return new this._$q((resolve, reject) => {
+      this.geocoder.geocode( { "address": `${location.address}, ${location.city}, ${location.state}` }, function(results) {
+            console.log(results);
+              let loc = results[0].geometry.location,
+                  lat      = loc.lat(),
+                  lng      = loc.lng();
+            console.log("Latitude: " + lat);
+            console.log("Longitude: " + lng);
+            this.locations.$add({
+              address: location.address,
+              city: location.city,
+              state: location.state,
+              coords: { lat, lng }
+            })
+            .then((ref) => {
+              resolve(this.locations);
+            })
+            .catch((error) => {
+              reject(error);
+            });
 
+      }.bind(this));
+    });
   }
-
-
-
-
 
 }
 
