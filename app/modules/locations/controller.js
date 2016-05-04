1d269594e59c6756879d0be613c03003ad93aa6b
Nancy Majors <nmajors@gmail.com>
2016-05-03 21:39:37 -0500

    geocoding, but getting same coordinates each time

--- app/modules/locations/controller.js
+++ app/modules/locations/controller.js
@@ -1,20 +1,31 @@
 class LocationsController {
-  constructor($state, UserService) {
+  constructor($state, UserService, LocationsService) {
     this._$state = $state;
+    this._LocationsService = LocationsService;
     this._UserService = UserService;
+    this.newLocation = this._LocationsService.new();
+
 
 
     this.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    this.markers = [];
 
     this._UserService
       .isLoggedIn()
       .then((response) => {
         this.user = response;
-        console.log(this.user);
+        // console.log(this.user);
+        this.locations = this._LocationsService.getLocations(this.user);
       })
       .catch((error) => {
         this._$state.go("login");
       });
+  }
+
+
+  addLocation() {
+    this._LocationsService.createLocation(this.newLocation);
+    console.log(this.newLocation);
   }
 
 
--- app/modules/locations/service.js
+++ app/modules/locations/service.js
@@ -1,9 +1,45 @@
 class LocationsService {
-  constructor(){
+  constructor($firebaseArray) {
+    this._$firebaseArray = $firebaseArray;
 
+    this.ref = new Firebase("https://nms-severe.firebaseio.com/");
+
+    this.geocoder = new google.maps.Geocoder();
 
 
   }
+
+  //get locations for logged in user
+  getLocations(user) {
+    this.locations = this._$firebaseArray(this.ref.child('users').child(user.uid).child('locations'));
+    return this.locations;
+  }
+
+  new() {
+    return {
+      address: "",
+      city: "",
+      state: ""
+      }
+  }
+
+  createLocation(location){
+    this.geocoder.geocode( { "address": 'location' }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+            let location = results[0].geometry.location,
+                lat      = location.lat(),
+                lng      = location.lng();
+          console.log("Latitude: " + lat);
+          console.log("Longitude: " + lng);
+          console.log(results);
+        }
+    });
+
+    this.locations.$add(location);
+  }
+
+
+
 
 
 }
