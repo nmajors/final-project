9fd9478d604a9386061645d1c29c781de2386515
Nancy Majors <nmajors@gmail.com>
2016-05-11 09:57:46 -0500

    finished demo mode, information displayed in the infoWindow matches condition displayed by marker

--- app/modules/locations/controller.js
+++ app/modules/locations/controller.js
@@ -17,7 +17,7 @@ class LocationsController {
 
 
 
-  $scope.onClick = function(marker, eventName, model) {
+    $scope.onClick = function(marker, eventName, model) {
       model.show = !model.show;
     };
 
@@ -36,40 +36,38 @@ class LocationsController {
       .then((response) => {
 
         this.user = response;
-        navigator.geolocation.getCurrentPosition((position ) => {
+        navigator.geolocation.getCurrentPosition((position) => {
           //add a marker to the user's current location.
           this.currentPosition = {
-              id: position.timestamp.toString(),
-              coords: {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-              },
-              options: {
-                icon: '../assets/images/star.png'
-              },
-              title: "Your current Location",
-              weather: {},
-              image: ""
+            id: position.timestamp.toString(),
+            coords: {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude
+            },
+            options: {
+              icon: '../assets/images/star.png'
+            },
+            title: "Your current Location",
+            weather: {},
+            image: ""
+          };
 
-            };
+          let latitude = position.coords.latitude;
+          let longitude = position.coords.longitude;
+          this._$http
+            .get(`http://whispering-everglades-16419.herokuapp.com/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial`)
+            .then((response) => {
+              console.log(response);
+              let weather = response.data;
+              let iconCode = response.data.weather[0].icon;
+              let iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
 
-            let latitude = position.coords.latitude;
-            let longitude = position.coords.longitude;
-            this._$http
-              .get(`http://whispering-everglades-16419.herokuapp.com/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial`)
-              .then((response) => {
-                console.log(response);
-                let weather = response.data;
-                let iconCode = response.data.weather[0].icon;
-                let iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+              this.currentPosition.weather = weather;
+              this.currentPosition.image = iconUrl;
 
-                this.currentPosition.weather = weather;
-                this.currentPosition.image = iconUrl;
-
-
-                console.log(this.currentPosition);
-              });
-            //ng-if on ui-gmap-marker prevents marker add attempts before geolocation is complete
+              console.log(this.currentPosition);
+            });
+          //ng-if on ui-gmap-marker prevents marker add attempts before geolocation is complete
         });
         this._LocationsService.getLocations(this.user)
           .then((response) => {
@@ -77,10 +75,10 @@ class LocationsController {
             this.showMarkers();
           });
 
-        })
-        .catch((error) => {
-          this._$state.go("login");
-        });
+      })
+      .catch((error) => {
+        this._$state.go("login");
+      });
 
   }
 
@@ -94,18 +92,19 @@ class LocationsController {
       });
   }
 
-deleteLocation(place){
-   this._LocationsService.removeLocation(place);
- }
+  deleteLocation(place) {
+    this._LocationsService.removeLocation(place);
+    this.showMarkers();
+  }
 
-changeLocation(place){
-  place.editing = true;
-}
+  changeLocation(place) {
+    place.editing = true;
+  }
 
-saveLocation(place) {
-  place.editing = false;
-  this._LocationsService.editLocation(place);
-}
+  saveLocation(place) {
+    place.editing = false;
+    this._LocationsService.editLocation(place);
+  }
 
 
   logout() {
@@ -119,52 +118,36 @@ saveLocation(place) {
   }
 
 
-
-
   getMarkerIcon(location) {
 
     let markerIcon = '../assets/images/';
     let locationWeatherCode = location.weather.weather[0].id;
 
-    // console.log(locationWeatherCode);
-
     if (this.demoMode) {
 
-      let demoCodes = [ 961, 531, 622, 771, 800, 804, 900, 902 ];
-       locationWeatherCode = demoCodes[Math.floor(Math.random() * demoCodes.length)];
-       console.log('demo mode ' + locationWeatherCode);
+      let demoCodes = [961, 531, 622, 771, 800, 804, 900, 902];
+      locationWeatherCode = demoCodes[Math.floor(Math.random() * demoCodes.length)];
+      console.log('demo mode ' + locationWeatherCode);
     }
 
-
-
-    if (locationWeatherCode >= 200 && locationWeatherCode <= 232 || locationWeatherCode === 960 || locationWeatherCode === 961){
+    if (locationWeatherCode >= 200 && locationWeatherCode <= 232 || locationWeatherCode === 960 || locationWeatherCode === 961) {
       markerIcon += 'thunderstorm.png';
 
-
-
-    }
-    else if (locationWeatherCode >= 300 && locationWeatherCode <= 531){
+    } else if (locationWeatherCode >= 300 && locationWeatherCode <= 531) {
       markerIcon += 'rain.png';
-    }
-    else if (locationWeatherCode >= 600 && locationWeatherCode <= 622){
+    } else if (locationWeatherCode >= 600 && locationWeatherCode <= 622) {
       markerIcon += 'snowy.png';
-    }
-    else if (locationWeatherCode >=700 && locationWeatherCode <=771){
+    } else if (locationWeatherCode >= 700 && locationWeatherCode <= 771) {
       markerIcon += 'haze.png';
-    }
-    else if (locationWeatherCode >= 801 && locationWeatherCode <= 804){
+    } else if (locationWeatherCode >= 801 && locationWeatherCode <= 804) {
       markerIcon += 'cloudy.png';
-    }
-    else if (locationWeatherCode === 800){
+    } else if (locationWeatherCode === 800) {
       markerIcon += 'sunny.png';
-    }
-    else if (locationWeatherCode === 781 || locationWeatherCode === 900){
+    } else if (locationWeatherCode === 781 || locationWeatherCode === 900) {
       markerIcon += 'tornado.png';
-    }
-    else if (locationWeatherCode === 901 || locationWeatherCode === 902){
+    } else if (locationWeatherCode === 901 || locationWeatherCode === 902) {
       markerIcon += 'hurricane.png';
-    }
-    else {
+    } else {
       markerIcon += 'default.png';
     }
     return markerIcon;
@@ -209,19 +192,48 @@ saveLocation(place) {
             }
           }
 
-          if (this.demoMode && this.marker.options.icon === '../assets/images/tornado.png' || this.marker.options.icon ==='../assets/images/hurricane.png') {
-          // if (this.locationWeatherCode === 781 || this.locationWeatherCode === 900){
+          if (this.demoMode && this.marker.options.icon === '../assets/images/tornado.png') {
             this.marker.options.animation = google.maps.Animation.BOUNCE;
             this.marker.image = 'http://openweathermap.org/img/w/11d.png';
-            this.marker.condition = "BONED";
+            this.marker.condition = "Tornado, seek shelter!";
           }
 
-          // if (this.marker.options.icon === '../assets/images/tornado.png') {
-          // // if (this.locationWeatherCode === 781 || this.locationWeatherCode === 900){
-          //   this.marker.options.animation = google.maps.Animation.BOUNCE;
-          //   this.marker.image = 'http://openweathermap.org/img/w/11d.png';
-          //   this.marker.condition = "BONED";
-          // }
+          if (this.demoMode && this.marker.options.icon === '../assets/images/hurricane.png') {
+            this.marker.options.animation = google.maps.Animation.BOUNCE;
+            this.marker.image = 'http://openweathermap.org/img/w/11d.png';
+            this.marker.condition = "Hurricane";
+          }
+
+          if (this.demoMode && this.marker.options.icon === '../assets/images/thunderstorm.png') {
+            this.marker.image = 'http://openweathermap.org/img/w/11d.png';
+            this.marker.condition = "Thunderstorm";
+          }
+
+          if (this.demoMode && this.marker.options.icon === '../assets/images/snowy.png') {
+            this.marker.image = 'http://openweathermap.org/img/w/13d.png';
+            this.marker.condition = "Snow";
+          }
+
+          if (this.demoMode && this.marker.options.icon === '../assets/images/rain.png') {
+            this.marker.image = 'http://openweathermap.org/img/w/09d.png';
+            this.marker.condition = "Rain";
+          }
+
+          if (this.demoMode && this.marker.options.icon === '../assets/images/haze.png') {
+            this.marker.image = 'http://openweathermap.org/img/w/50d.png';
+            this.marker.condition = "Haze";
+          }
+
+          if (this.demoMode && this.marker.options.icon === '../assets/images/cloudy.png') {
+            this.marker.image = 'http://openweathermap.org/img/w/04d.png';
+            this.marker.condition = "Cloudy";
+          }
+
+          if (this.demoMode && this.marker.options.icon === '../assets/images/sunny.png') {
+            this.marker.image = 'http://openweathermap.org/img/w/01d.png';
+            this.marker.condition = "Clear Sky";
+          }
+
           console.log(this.marker);
           this.markers.push(this.marker);
 
