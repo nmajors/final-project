184591c22b4c0d75d8c1e5b61014c2a8714416ae
Nancy Majors <nmajors@gmail.com>
2016-05-18 09:07:41 -0500

    Update 2016-05-18T14:07:18.517Z

--- app/modules/locations/config.js
+++
@@ -1,9 +0,0 @@
-function config($stateProvider) {
-  $stateProvider
-  .state('locations', {
-    url: '/',
-    controller: 'LocationsController as locationsCtrl',
-    template: require('./view.html')
-  })
-}
- export default config;
